1ad9ff2dcb413f0a041c36a2bf98a55ee51b72cf
david yunseong jung <145014381+yjkai27@users.noreply.github.com>
2023-09-22 18:12:49 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+* {
+  transition: all 0.5s ease-in-out;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+  padding: 20px;
+}
+
+.quote-box {
+  background-color: #f5f5f5;
+  padding: 20px;
+  margin-bottom: 20px;
+  font-family: serif;
+  border-radius: 10px;
+}
+
+#quote {
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+#author {
+  font-size: 18px;
+  font-style: italic;
+}
+
+button {
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  font-size: 18px;
+  cursor: pointer;
+  font-family: serif;
+  border-radius: 5px;
+}
+.made-by {
+  color: white; 
+}
